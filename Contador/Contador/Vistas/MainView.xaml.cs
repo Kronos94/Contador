4a2ab74756a1be9f40674803f8c5bcc9d0c29a97
Antonio Benitez <antoniobenitezm.f@gmail.com>
2017-10-25 19:32:47 +0200

    Contador terminado.

---
+++ Contador/Contador/Vistas/MainView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Contador.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainView : ContentPage
+    {
+
+        public int contador = 0;
+
+        public MainView()
+        {
+            InitializeComponent();
+
+
+            btnAumentar.Clicked += (sender, args) =>
+            {
+
+                lblTexto.Text = String.Format("Has pulsado el boton {0} veces", ++contador);
+
+            };
+        }
+    }
+}
